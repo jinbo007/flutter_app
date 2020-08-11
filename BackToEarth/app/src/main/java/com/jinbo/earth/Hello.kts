10d90d0ea853adb4454c415cb0da055f6116da23
侯金博 <houjinbo@jlpay.com>
2020-08-11 12:21:42 +0800

    Initial commit

---
+++ BackToEarth/app/src/main/java/com/jinbo/earth/Hello.kts
@@ -0,0 +1,9 @@
+var languare  :String   = "kotlin"
+
+
+if(count = 12){
+    print("i have the answer")
+
+}else{
+    println("the answer eludes me")
+}
---
+++ android/app/src/main/kotlin/com/jinbo/flutterapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jinbo.flutterapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
